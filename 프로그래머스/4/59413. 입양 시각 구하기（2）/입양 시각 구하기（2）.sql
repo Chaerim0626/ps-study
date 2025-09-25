b1d6da39a6682dc6691c7c3e0038f8ebe7deba30
Chaerim Park <65753465+Chaerim0626@users.noreply.github.com>
2025-09-25 21:40:27 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -0,0 +1,22 @@
+
+-- 빈 시간은 0으로 채워야함 
+-- 몇시에 가장 활발? 
+
+WITH RECURSIVE tmp AS (
+    SELECT 0 AS num
+    UNION ALL
+    SELECT num+1
+    FROM tmp
+    WHERE num < 23
+)
+SELECT tmp.num as HOUR, COALESCE(a.COUNT,0) as COUNT from tmp LEFT JOIN
+(
+    select HOUR(A.DATETIME) as HOUR,
+    COUNT(*) as COUNT
+    FROM ANIMAL_OUTS AS A
+    GROUP BY HOUR
+) AS A
+ON tmp.num = A.HOUR
+GROUP BY tmp.num
+ORDER BY tmp.num ASC;
+
