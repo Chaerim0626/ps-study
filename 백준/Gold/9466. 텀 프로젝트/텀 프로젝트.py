79ac71a95d4fb867832ce47d8eb0206c363d762a
Chaerim Park <65753465+Chaerim0626@users.noreply.github.com>
2025-10-29 10:04:37 +0900

    [Gold III] Title: 텀 프로젝트, Time: 2592 ms, Memory: 58896 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -1,45 +1,34 @@
 import sys
-sys.setrecursionlimit(10**7)
 input = sys.stdin.readline
 
-def dfs(x):
-    global cycle_count
-    visited[x] = 1
-    nxt = nums[x]
+sys.setrecursionlimit(10**6) 
 
-    if not visited[nxt]:      # 아직 방문 안 했으면 계속 DFS
-        dfs(nxt)
+def dfs(i):
+    global count 
+
+    visited[i] = 1
+    cycle_list.append(i)
+
+    if visited[nums[i]]:
+        if nums[i] in cycle_list:
+            count -= len(cycle_list[cycle_list.index(nums[i]):])
+        return 
     else:
-        if not done[nxt]:     # ✅ 아직 처리 완료 안 되었으면 사이클
-            cur = nxt
-            while True:
-                cycle_count += 1   # ✅ 사이클 노드만 count
-                cur = nums[cur]
-                if cur == nxt:
-                    break
-
-    done[x] = 1  # ✅ 처리 완료 표시
-
+        dfs(nums[i])
 t = int(input())
 for _ in range(t):
     n = int(input())
-    nums = [0] + list(map(int, input().split()))
+
+    nums = [0]
+    nums.extend([int(x) for x in input().split()])
 
     visited = [0] * (n+1)
-    done = [0] * (n+1)
-    cycle_count = 0
+    count = n
 
-    # 1) 자기 자신 루프 먼저 처리
-    for i in range(1, n+1):
-        if nums[i] == i:
-            cycle_count += 1
-            done[i] = 1
-            visited[i] = 1
-
-    # 2 ) 그 외 DFS 로 사이클만 찾기
-    for i in range(1, n+1):
+    for i in range(1,n+1):
         if not visited[i]:
+            cycle_list = []
             dfs(i)
-
-    # 팀 못 이루는 사람 출력
-    print(n - cycle_count)
+            #print(cycle_list)
+    print(count)
+        
