34e2c2b0e1b639c913e0fa377f50b458352ea677
Chaerim Park <65753465+Chaerim0626@users.noreply.github.com>
2025-12-18 15:31:22 +0900

    [level 2] Title: 주식가격, Time: 26.54 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,17 +1,18 @@
 from collections import deque
 
 def solution(prices):
-    dq = deque(prices)
-    result = []
     
-    while dq:
-        cur_price = dq.popleft()
-        time = 0
-        
-        for i in dq:
-            time += 1
-            if cur_price > i:
-                break 
-        result.append(time)
-        
+    n = len(prices)
+    result = [0] * n 
+    st = [] # 인덱스
+    
+    for i, price in enumerate(prices):
+        while st and prices[st[-1]] > price:
+            j = st.pop()
+            result[j] = i-j 
+        st.append(i)
+    
+    while st:
+        i = st.pop()
+        result[i] = (n-1)-i
     return result
