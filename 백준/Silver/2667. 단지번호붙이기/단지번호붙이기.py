da9cc73366701a18666f3a5abd2ba47a64e32a2e
Chaerim Park <65753465+Chaerim0626@users.noreply.github.com>
2026-01-19 20:32:26 +0900

    [Silver I] Title: 단지번호붙이기, Time: 56 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,43 +1,47 @@
+import sys
+input = sys.stdin.readline
 from collections import deque
 
-graph = []
 n = int(input())
-for _ in range(n):
-    s = list(map(int,input()))
-    graph.append(s)
-
+graph = []
 visited = [[0] * n for _ in range(n)]
+
+for _ in range(n):
+    row = list(map(int,input().rstrip()))
+    graph.append(row)
 
 def isvalid(x,y):
     return 0 <= x < n and 0 <= y < n
-    
-def bfs(sx,sy):
-    dq = deque([(sx,sy)])
-    visited[sx][sy] = 1
-    dir = [(0,1), (0,-1), (1,0), (-1,0)]
+
+def bfs(x,y):
     cnt = 1
-    
+    dir = [(0,1), (0,-1),(1,0), (-1,0)]
+    dq = deque([(x,y)])
+
     while dq:
         cx,cy = dq.popleft()
-            
-        for dx,dy in dir:
+
+        for dx, dy in dir:
             nx,ny = cx+dx, cy+dy
-            if isvalid(nx,ny) and not visited[nx][ny]:
-                if graph[nx][ny]:
-                    visited[nx][ny] =1 
-                    dq.append((nx,ny))
+
+            if isvalid(nx,ny):
+                if not visited[nx][ny] and graph[nx][ny]:
+                    visited[nx][ny] = 1
                     cnt += 1
+                    dq.append((nx,ny))
                     
     return cnt
-
+   
 result = []
 for i in range(n):
     for j in range(n):
-        if graph[i][j] and not visited[i][j]:
-            result.append(bfs(i,j))
-            
-print(len(result))
+        if not visited[i][j] and graph[i][j]:
+            visited[i][j] = 1
+            r = bfs(i,j)
+            result.append(r)
+
 result.sort()
+print(len(result))
 for i in result:
     print(i)
-    
+           
