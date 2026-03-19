60f2d52d8c311dfc78e80160e1e4505b5da1cefd
Chaerim Park <65753465+Chaerim0626@users.noreply.github.com>
2026-03-19 19:23:47 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 428 ms, Memory: 41868 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -1,58 +1,41 @@
-import sys
+import sys 
 input = sys.stdin.readline 
 from collections import deque 
+
+
 n,m = map(int,input().split())
 graph = []
+visited = [[-1]* m for _ in range(n)]
+for _ in range(n):
+    row = list(map(int,input().split()))
+    graph.append(row)
 
+        
 def isvalid(x,y):
     return 0 <= x < n and 0 <= y < m
-    
-for _ in range(n):
-    nums = list(map(int,input().split()))
-    graph.append(nums)
-    
 def bfs(sx,sy):
-    global graph 
-    
-    visited = [[0]*m for _ in range(n)]
-    dq = deque()
-    dq.append((sx,sy))
-    dir = [(0,1), (0,-1), (1,0), (-1,0)]
+    dir = [[0,1], [0,-1], [1,0], [-1,0]]
+    dq = deque([(sx,sy)])
 
-    visited[sx][sy] = 1
-    
+    visited[sx][sy] = 0
+
     while dq:
         cx,cy = dq.popleft()
 
         for dx,dy in dir:
-            nx,ny = cx+dx, cy+dy
-            if isvalid(nx,ny) and not visited[nx][ny]:
-                if cy == sy and cx == sx:
-                    if graph[nx][ny]:
-                        dq.append((nx,ny))
-                        visited[nx][ny] = 1
-                        continue 
-                elif graph[nx][ny] != 0:
-                    graph[nx][ny] = graph[cx][cy] + 1
-                    visited[nx][ny] = 1
+            nx,ny = dx+cx,dy+cy
+            if isvalid(nx,ny):
+                if visited[nx][ny] == -1 and graph[nx][ny]:
                     dq.append((nx,ny))
-
-    return graph, visited
-            
+                    visited[nx][ny] = visited[cx][cy] + 1
 
 for i in range(n):
     for j in range(m):
-        if graph[i][j] == 2:
-            x,y = i,j 
-            break 
-result, result_v = bfs(x,y)
-graph[x][y] = 0
+        if graph[i][j]==2:
+            sx,sy = i,j
+        elif graph[i][j] == 0:
+            visited[i][j] = 0
 
-for i in range(n):
-    for j in range(m):
-        if not result_v[i][j] and graph[i][j]:
-            graph[i][j] = -1
-            
-for row in graph:
+bfs(sx,sy)
+for row in visited:
     print(*row)
-        
